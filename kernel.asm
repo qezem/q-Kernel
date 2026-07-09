b10194427d9e29225c368c367c44408d89883aaa
nikika-mir0 <nmironov327@gmail.com>
2026-07-09 23:42:56 +0500

    Add Protect Mode

--- kernel.asm
+++ kernel.asm
@@ -1,15 +1,21 @@
 [bits 16]
-[org 0x0000]
+[org 0x8000]
 
 start:
-  mov ax, cs
+  xor ax, ax
   mov ds, ax
   mov es, ax
 
   mov si, msg
   call print
+  
+  cli
+  lgdt [ds:GDT_Descriptor]
+  mov eax, cr0
+  or eax, 1
+  mov cr0, eax
 
-  jmp $
+  jmp CODE_SEG:start_protected_mode
 
 print:
   lodsb
@@ -23,6 +29,26 @@ print:
   jmp print
 .done:
   ret
+
+%include "gdt.asm"
+
+[bits 32]
+start_protected_mode:
+  mov ax, DATA_SEG
+  mov ds, ax
+  mov ss, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+
+  mov esp, 0x9000
+
+  mov al, 'A'
+  mov ah, 0x0f
+
+  mov [0xb8000], ax
+
+  jmp $
 
 msg: db 'Kernel is ready!', 0x0d, 0x0a, 0
 
